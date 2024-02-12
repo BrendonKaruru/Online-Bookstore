be58c15ebe185da9e5ab2c1ee6938cd3fefb2604
BrendonKaruru <brendonkaruru21@gmail.com>
2024-02-12 14:37:57 +0200

    edits

--- js/script.js
+++ js/script.js
@@ -37,3 +37,19 @@ window.onload = () => {
 
     }
 }
+
+var swiper = new Swiper(".books-slider", {
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+
+});
